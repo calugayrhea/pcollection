0dbe8a9d2b2bc7c88620e4fd1ba0d96a3657f9c0
Rhea Calugay <rheadubricocalugay@gmail.com>
2023-09-28 17:32:23 +0800

    connect backend to frontend

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const collectionRoutes = require('../backend/routes/collectionRoute');
 const photoRoutes = require('./routes/photoRoutes');
--- frontend/pcollection/src/App.vue
+++ frontend/pcollection/src/App.vue
@@ -1,47 +1,33 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Collections</h1>
+    <ul>
+      <li v-for="collection in collections" :key="collection.id">
+        ID: {{ collection.id }} | Name: {{ collection.name }} | Email: {{ collection.email }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import api from '@/plugins/api';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  data() {
+    return {
+      collections: [],
+    };
+  },
+  async mounted() {
+    try {
+      // Fetch collections data from the backend
+      this.collections = await api.getAllCollections();
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+      // Check if data is fetched
+      console.log('Collections:', this.collections);
+    } catch (error) {
+      console.error('Error fetching collections:', error);
+    }
+  },
+};
+</script>
