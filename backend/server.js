ab858faf6a4943f94f6b4ccf0510f93bf98a556a
Rhea Calugay <rheadubricocalugay@gmail.com>
2023-10-09 23:47:24 +0800

    add jest test

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000; // Use process.env.PORT for Heroku or other hosting platforms
+const port = process.env.PORT || 3000; 
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -11,17 +11,13 @@ app.use(cors());
 const collectionRoutes = require('./routes/collectionRoute');
 app.use('/api', collectionRoutes);
 
-// Define the server start function
 const startServer = () => {
   return app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
 };
-
-// If this file is the main module, start the server
 if (require.main === module) {
   startServer();
 }
 
-// Export the app and startServer function for testing
 module.exports = { app, startServer };
