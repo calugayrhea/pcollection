9afd2cf53efc05361fe1ce117181b3f8782dddc8
Rhea Calugay <rheadubricocalugay@gmail.com>
2023-10-09 23:45:43 +0800

    add jest test

--- backend/config/httpStatuscode.js
+++ backend/config/httpStatuscode.js
@@ -7,6 +7,7 @@ module.exports = {
     UNAUTHORIZED: 401,
     FORBIDDEN: 403,
     NOT_FOUND: 404,
+    NO_CONTENT: 204,
     SERVER_ERROR: 500,
   };
   
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
 const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000; // Use process.env.PORT for Heroku or other hosting platforms
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -10,6 +11,17 @@ app.use(cors());
 const collectionRoutes = require('./routes/collectionRoute');
 app.use('/api', collectionRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Define the server start function
+const startServer = () => {
+  return app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+// If this file is the main module, start the server
+if (require.main === module) {
+  startServer();
+}
+
+// Export the app and startServer function for testing
+module.exports = { app, startServer };
